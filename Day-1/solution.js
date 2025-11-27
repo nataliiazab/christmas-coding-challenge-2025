132409f258c29769fae601c559158223ed0dc1f3
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2025-11-27 22:21:25 +0000

    complete day 1 challenge

---
+++ Day-1/solution.js
@@ -0,0 +1,15 @@
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+
+// Step 1: Iterate over the numbers in the array - let i be the current index
+// Step 2: For each number, iterate over the rest of the numbers in the array- let j be the index after i
+// Step 3: Check if the current two numbers add up to the target - if (nums[i] + nums[j] === target)
+// Step 4: If so, return the array [i, j] containing the indices of the two numbers
